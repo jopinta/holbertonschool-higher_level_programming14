3f77f5f737ed5406be26278b7f902b912afd7c57
jopinta <tatinfa@yahoo.com>
2021-05-06 20:49:55 +0000

    uniqlo

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def uniq_add(mylist=[]):
-    new-list = set(my_list)
+def uniq_add(my_list=[]):
+    new_list = set(my_list)
     result = 0
     for i in new_list:
-        result = += i
+        result += i
     return result
