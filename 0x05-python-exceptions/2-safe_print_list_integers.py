6908d9cd2010b11bf019cec179605e7f112ff4e6
jopinta <tatinfa@yahoo.com>
2021-05-17 23:43:20 +0000

    block

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+
+def safe_print_list_integers(my_list=[], x=0):
+
+    count = 0
+
+    while i < x:
+
+        try:
+            print("{:d}".format(my_list[i]), end="")
+
+            count += 1:
+
+        except (TypeError, ValueError):
+
+            continue
+
+print("")
+
+return count
