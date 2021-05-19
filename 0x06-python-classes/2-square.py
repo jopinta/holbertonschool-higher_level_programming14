807f0788f07c8317df330f2520aafbad412acd1d
jopinta <tatinfa@yahoo.com>
2021-05-19 01:53:04 +0000

    racing

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,6 +6,12 @@ class Square:
     """class Square that defines a square"""
     __size = None
 
-    def __init__ (self, size):
-        """Instation with size"""
-        self.__size = size
+    def __init__(self, size=0):
+        """Instantiation with optional size"""
+        if type(size) is int:
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
