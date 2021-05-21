fc6576254e9286d90792d959ecfd2162338e60ad
jopinta <tatinfa@yahoo.com>
2021-05-21 23:40:55 +0000

    dz

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -7,15 +7,13 @@ Returns an integer: the addition of a and b
 You are not allowed to import any module
 """
 
-def add_integer(a, b=98):
-""" function thats adds two integers."""
 
-    for i in range(a, b):
-    if i =! range(a, b)
-    raise
-    TypeError a must be an integer or b must be an integer
-    if type(a) is float:
-        a = int(a)
-    if type(b) is float
-        b = int(b)
-    return (a + b)
+def add_integer(a, b=98):
+    """ function thats adds two integers."""
+
+    if (not isinstance(a, int) and not isinstance(a, float)):
+        raise TypeError("a must be an integer")
+    if (not isinstance(b, int) and not isinstance(b, float)):
+        raise TypeError("b must be an integer")
+
+    return int(a + b)
---
+++ 0x07-python-test_driven_development/cm.py
@@ -0,0 +1,9 @@
+def cm(feet = 0, inches = 0):
+"""Converts a length from feet and inches"""
+
+    inches_to_cm = inches * 2.54
+    feet_to_cm = feet * 12 * 2.54
+    return inches_to_cm + feet_to_cm
+
+
+     
