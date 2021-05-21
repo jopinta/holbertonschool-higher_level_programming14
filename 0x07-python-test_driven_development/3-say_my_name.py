2c8017a789e403d3daa0497d385e4fd3d00eb6a7
jopinta <tatinfa@yahoo.com>
2021-05-21 23:43:16 +0000

    as

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" test this function in
+    /test/3-say_my_name.txt
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """print a name
+        usage: say_my_name(str, str)
+    """
+
+    if (type(first_name) != str):
+        raise TypeError("first_name must be a string")
+    if (type(last_name) != str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
