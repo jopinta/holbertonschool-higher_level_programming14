5a23e551b06b29a4db7c2c94265e8b01bd510b5e
jopinta <tatinfa@yahoo.com>
2021-05-24 23:28:41 +0000

    algo fatal

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+
+class Rectangle:
+
+    def __init__(self, width=0, height=0):
+        """ function to define a rectangle"""
+        self.__width = width
+        self.__height = height
+
+    @property
+    def height(self):
+        """ kkksdknfks"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ escribir algo"""
+        if type(value) == int
+        self.__height = value
+
+    elif
+    raise ValueError("height must be >= 0")
+
+    @property
+    def width(self):
+        """puede ser"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """property to set with"""
+        if type(value) != int
+        raise TypeError("must be an integer")
+
+    elif value < 0:
+        raise ValueError("must be >= 0")
