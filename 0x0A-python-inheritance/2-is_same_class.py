e4a10461a7a7d3fdea2dc8cdfde28e5a08bc5fb6
jopinta <tatinfa@yahoo.com>
2021-06-01 23:58:35 +0000

    instance

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """the object is exactly"""
 
-def is_same_class(obj, a_class):
-"""will print true or false by default"""
 
+def is_same_class(obj, a_class):
+    """will print true or false by default"""
 
     return (type(obj) is a_class)
