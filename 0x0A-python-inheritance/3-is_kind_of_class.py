73ba05b9cd887edb68faf37879addf8e7baea617
jopinta <tatinfa@yahoo.com>
2021-06-02 00:03:44 +0000

    isinst

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!usr/bin/python3
+
+"""object is an instance of a class that inherited from"""
+
+
+def is_kind_of_class(obj, a_class):
+    """function that returns True"""
+
+    return (isinstance(obj, a_class))
