8cdf3112048a48b17b37adad79db76a5cace3c6b
jopinta <tatinfa@yahoo.com>
2021-06-02 23:19:44 +0000

    utf8

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,5 +5,5 @@
 def write_file(filename="", text=""):
     """function should overwrite the content of the file"""
 
-    with open("filename", mode='w', encoding="utf-8") as my_file:
+    with open("filename", mode='w', encoding='utf-8') as my_file:
         return(my_file.write(text))
