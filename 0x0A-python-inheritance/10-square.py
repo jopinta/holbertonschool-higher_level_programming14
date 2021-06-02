aa0f0fcc2f9d3e8004897fb545283d7f31e022bd
jopinta <tatinfa@yahoo.com>
2021-06-02 02:57:18 +0000

    hello

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""square stuff"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """square restro"""
+    def __init__(self, size):
+
+        """constructor"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
