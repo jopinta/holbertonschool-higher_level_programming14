6e2954b9ab2895240c019e866dcb50be62d1a167
jopinta <tatinfa@yahoo.com>
2021-06-02 00:44:54 +0000

    exito

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Public instance method"""
+
+
+class BaseGeometry:
+    """need to comment"""
+
+    def area(self):
+        """to calculate the area"""
+    if !0 area
+raise Exception "area() is not implemented"
