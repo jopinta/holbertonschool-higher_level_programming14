6bc5991adcfaeb14570f5ce76f5306253ebd3dcc
jopinta <tatinfa@yahoo.com>
2021-06-02 23:06:58 +0000

    moo

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,5 @@
 def read_file(filename=""):
     """ use the with statement"""
 
-    with open(filename, 'r', encoding="utf-8") as my_file:
-        print(my_file.read())
+    with open(filename, mode='r', encoding="utf-8") as my_file:
+        print(my_file.read(), end="")
