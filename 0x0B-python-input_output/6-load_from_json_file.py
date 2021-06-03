01ca0bd01c418fb76650b3c39926f0ba564f445a
jopinta <tatinfa@yahoo.com>
2021-06-03 00:40:13 +0000

    fore

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""function that creates an Object from JSON file"""
+
+import json
+
+
+def load_from_json_file(filename):
+    """t need to manage exceptionsdon"""
+
+    with open(filename) as fp
+    return (json.load(fp.read()))
