58133f03ae813020e2cf8b95996e6df5a0bd72db
jopinta <tatinfa@yahoo.com>
2021-06-03 03:00:30 +0000

    pausa

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -5,7 +5,7 @@
 class Student:
     """new class"""
 
-    def __init__(self, first_name, lastname, age):
+    def __init__(self, first_name, last_name, age):
         """constructor fun"""
         self.first_name = first_name
         self.last_name = last_name
